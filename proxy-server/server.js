38f05f72c50a6dc8cfda58ef493902d27d998d41
vausa24434 <muvausastore1@gmail.com>
2024-08-09 13:45:12 +0700

    add data product

---
+++ proxy-server/server.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const axios = require("axios");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const app = express();
+const port = 3001;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.post("/price-list", async (req, res) => {
+  try {
+    const response = await axios.post(
+      "https://api.digiflazz.com/v1/price-list",
+      req.body,
+      {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    res.json(response.data);
+  } catch (error) {
+    console.error("Error fetching price list:", error);
+    res.status(500).json({ error: "Error fetching price list" });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Proxy server running at http://localhost:${port}`);
+});
---
+++ src/Pages/Login.jsx
@@ -0,0 +1,67 @@
+import { useState } from 'react';
+import { Link, useNavigate} from 'react-router-dom';
+import { supabase } from '../client';
+
+export default function Login({setToken}) {
+  let navigate = useNavigate()
+
+  const [formData,setFormData] = useState({
+        email:'',password:''
+  })
+
+  console.log(formData)
+
+  function handleChange(event){
+    setFormData((prevFormData)=>{
+      return{
+        ...prevFormData,
+        [event.target.name]:event.target.value
+      }
+    })
+  }
+  async function handleSubmit(e){
+    e.preventDefault()
+    try {
+        const { data, error } = await supabase.auth.signInWithPassword({
+            email: formData.email,
+            password: formData.password,
+          })
+
+      if (error) throw error
+      console.log(data)
+      setToken(data)
+      navigate('/')
+      
+    } catch (error) {
+      alert(error)
+    }
+  }
+
+  return (
+    <div>
+    <form onSubmit={handleSubmit}>
+      
+
+      <input 
+        placeholder='Email'
+        name='email'
+        onChange={handleChange}
+      />
+
+      <input 
+        placeholder='Password'
+        name='password'
+        type="password"
+        onChange={handleChange}
+      />
+
+      <button type='submit'>
+        Submit
+      </button>
+
+
+    </form>
+    Don't have an account? <Link to='/register'>Sign Up</Link> 
+  </div>
+  )
+}
