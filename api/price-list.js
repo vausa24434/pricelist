19bf5e72a34e04a95b24b000bac1bea8094d7878
vausa24434 <muvausastore1@gmail.com>
2024-08-09 13:13:20 +0700

    add folder api

--- api/price-list.js
+++ api/price-list.js
@@ -1,10 +1,24 @@
 import axios from 'axios';
 
 export default async function handler(req, res) {
-  try {
-    const response = await axios.get('https://api.digiflazz.com/v1/price-list');
-    res.status(200).json(response.data);
-  } catch (error) {
-    res.status(500).json({ error: 'Error fetching price list' });
+  if (req.method === 'POST') {
+    try {
+      const response = await axios.post(
+        'https://api.digiflazz.com/v1/price-list',
+        req.body,
+        {
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        }
+      );
+      res.status(200).json(response.data);
+    } catch (error) {
+      console.error('Error fetching price list:', error.message);
+      res.status(500).json({ error: 'Error fetching price list' });
+    }
+  } else {
+    res.setHeader('Allow', ['POST']);
+    res.status(405).end(`Method ${req.method} Not Allowed`);
   }
 }
