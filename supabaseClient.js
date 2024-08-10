cfcdae3ae95c6dc4804e8b9701f7aabd674f7e2b
vausa24434 <muvausastore1@gmail.com>
2024-08-10 08:36:19 +0700

    add env variables

--- supabaseClient.js
+++ supabaseClient.js
@@ -6,6 +6,6 @@
 // export const supabase = createClient(supabaseUrl, supabaseKey)
 
 import { createClient } from '@supabase/supabase-js'
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URLL;
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
 export const supabase = createClient(supabaseUrl, supabaseKey)
