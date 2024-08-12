589f0347f03b64e766c95a3b02e419b43f91e93a
vausa24434 <muvausastore1@gmail.com>
2024-08-12 19:25:50 +0700

    set env

--- src/Components/Auth/AuthProvider.jsx
+++ src/Components/Auth/AuthProvider.jsx
@@ -1,5 +1,5 @@
 import { createContext, useContext, useEffect, useState } from 'react';
-import { supabase } from '../../../supabaseClients';
+import { supabase } from '../../../supabaseClient';
 
 const AuthContext = createContext();
 
--- supabaseClients.js
+++
@@ -1,4 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-const supabaseUrl = process.env.VITE_SUPABASE_URL;
-const supabaseKey = process.env.VITE_SUPABASE_KEY;
-export const supabase = createClient(supabaseUrl, supabaseKey)
