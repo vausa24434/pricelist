f48169183a52b28fc86fbcae67dba33c1c5904b9
vausa24434 <muvausastore1@gmail.com>
2024-08-12 18:55:54 +0700

    change supabase client

--- supabaseClient.js
+++ supabaseClient.js
@@ -1,9 +1,9 @@
-import { createClient } from '@supabase/supabase-js'
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
-export const supabase = createClient(supabaseUrl, supabaseKey)
-
 // import { createClient } from '@supabase/supabase-js'
-// const supabaseUrl = process.env.VITE_SUPABASE_URL;
-// const supabaseKey = process.env.VITE_SUPABASE_KEY;
+// const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+// const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
 // export const supabase = createClient(supabaseUrl, supabaseKey)
+
+import { createClient } from '@supabase/supabase-js'
+const supabaseUrl = process.env.VITE_SUPABASE_URL;
+const supabaseKey = process.env.VITE_SUPABASE_KEY;
+export const supabase = createClient(supabaseUrl, supabaseKey)
