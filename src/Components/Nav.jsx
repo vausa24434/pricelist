c40f2675384aee38701b818e91f0466320ded7e7
vausa24434 <muvausastore1@gmail.com>
2024-08-12 13:20:26 +0700

    set navbar

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -57,35 +57,10 @@ export default function Nav() {
 
           <div className="hidden items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-sticky">
             <ul className="flex flex-col gap-x-1 p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-utama">
-            <li>
-                                <NavLink className={({ isActive }) => (isActive ? activeLink : nonActiveLink)} to='/'>Beranda</NavLink>
-                            </li>
-                            <li>
-                                <NavLink className={({ isActive }) => (isActive ? activeLink : nonActiveLink)} to='/update'>Update</NavLink>
-                            </li>
+              <li>
+                <NavLink className={({ isActive }) => (isActive ? activeLink : nonActiveLink)} to='#'>About Us</NavLink>
+              </li>
             </ul>
-          </div>
-          {/* Desktop Menu */}
-
-          <div className="hidden sm:block  flex md:order-2 items-center space-x-4 ">
-
-            <button onClick={() => navigate("/login")} className="flex items-center bg-utama text-white font-normal rounded-full text-sm px-5 py-2">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="w-6 h-6 mr-2"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M20 12H8m12 0-4 4m4-4-4-4M9 4H7a3 3 0 0 0-3 3v10a3 3 0 0 0 3 3h2"
-                />
-              </svg>
-              <p className="p-2">Login</p>
-            </button>
           </div>
 
           {/* Mobile Menu */}
@@ -115,29 +90,7 @@ export default function Nav() {
                   <h2 className="text-xl font-bold">Menu</h2>
                   <ul className="mt-4">
                     <li>
-                      <NavLink className={({ isActive }) => (isActive ? 'block py-2 px-4 bg-gray-700 rounded' : 'block py-2 px-4 hover:bg-gray-700 rounded')} to='/'>Beranda</NavLink>
-                    </li>
-                    <li>
-                      <NavLink className={({ isActive }) => (isActive ? 'block py-2 px-4 bg-gray-700 rounded' : 'block py-2 px-4 hover:bg-gray-700 rounded')} to='/update'>Update</NavLink>
-                    </li>
-                    <li>
-                      <button onClick={() => navigate("/login")} className="flex items-center py-12 px-4 text-gray-300 hover:text-white">
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          className="w-6 h-6 mr-2"
-                          fill="none"
-                          viewBox="0 0 24 24"
-                          stroke="currentColor"
-                        >
-                          <path
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            strokeWidth="2"
-                            d="M20 12H8m12 0-4 4m4-4-4-4M9 4H7a3 3 0 0 0-3 3v10a3 3 0 0 0 3 3h2"
-                          />
-                        </svg>
-                        Login
-                      </button>
+                      <NavLink className={({ isActive }) => (isActive ? 'block py-2 px-4 bg-gray-700 rounded' : 'block py-2 px-4 hover:bg-gray-700 rounded')} to='#'>About Us</NavLink>
                     </li>
                   </ul>
                 </div>
@@ -150,8 +103,3 @@ export default function Nav() {
     </>
   );
 }
-
-
-
-
-
