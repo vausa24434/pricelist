de2592fe2193c76e76dd973a08db4a9bc03a8d4c
vausa24434 <muvausastore1@gmail.com>
2024-08-13 11:10:04 +0700

    add qris page

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,6 @@ import Pencarian from './Pages/Search';
 import SearchLocalPage from './Pages/SearchLocal';
 import ProductPage from './Pages/Product';
 import ProductLocalPage from './Pages/ProductLocal';
-import QRISDinamisToStatis from './Pages/QRISDinamisToStatis';
 
 
 function App() {
@@ -38,7 +37,6 @@ function App() {
           <Route path="/local" element={<SearchLocalPage />} />
           <Route path="/productlocal/:id" element={<ProductLocalPage />} />
           <Route path="/update" element={<ProtectedRoute><UpdatePriceList setToken={setToken} /></ProtectedRoute>} />
-          <Route path="/qris" element={<QRISDinamisToStatis />} />
         </Routes>
       </BrowserRouter>
     </AuthProvider>
