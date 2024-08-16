c49aa631429590b77e29cacd1547589957023091
vausa24434 <muvausastore1@gmail.com>
2024-08-16 19:07:59 +0700

    change client

--- src/Components/Auth/AuthProvider.jsx
+++ src/Components/Auth/AuthProvider.jsx
@@ -1,8 +1,5 @@
 import { createContext, useContext, useEffect, useState } from 'react';
-import { createClient } from '@supabase/supabase-js'
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
-const supabase = createClient(supabaseUrl, supabaseKey)
+import { supabase } from '../../../supabaseClient';
 
 const AuthContext = createContext();
 
